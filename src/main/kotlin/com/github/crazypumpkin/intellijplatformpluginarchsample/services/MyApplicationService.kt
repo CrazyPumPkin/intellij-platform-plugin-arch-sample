5a9d1f30ed4be173c7200bfb1beac96c7a4025ec
GitHub Action <action@github.com>
2022-07-27 09:18:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/crazypumpkin/intellijplatformpluginarchsample/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.crazypumpkin.intellijplatformpluginarchsample.services
+
+import com.github.crazypumpkin.intellijplatformpluginarchsample.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
